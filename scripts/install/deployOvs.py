cd0333866e1f59143a90fe2bcf3cd84674fe836c
saelbrec <saelbrec@localhost>
2014-02-25 13:12:16 +0100

    OVS-405: Remove vmkernel portgroups from public and private network list to choose from

--- scripts/install/deployOvs.py
+++ scripts/install/deployOvs.py
@@ -299,6 +299,16 @@ class VMwareSystem():
         return portgroupsresult
 
     @staticmethod
+    def list_vmkernel_ports():
+        """
+        List the current vmkernel ports
+        """
+        vmkernelports_result = {}
+        _, interfaceout = InstallHelper.execute_command(['esxcli', '--formatter=keyvalue', 'network', 'ip', 'interface', 'list'])
+        vmkernel_interfaces = InstallHelper.convert_keyvalue(''.join(interfaceout))
+        return vmkernel_interfaces
+
+    @staticmethod
     def build_nic_config(nics):
         """
         Build the nic vmx content for the required nics and assign them with semi-random MAC addresses
@@ -490,15 +500,16 @@ if __name__ == '__main__':
     if not vswitches:
         print InstallHelper.boxed_message(['No portgroups found in your ESXi network configuration'])
         sys.exit(1)
-    available_pgs = VMwareSystem.list_vm_portgroups()
-    pg_names = available_pgs.keys()
-    if len(pg_names) < 2:
+    all_pgs = VMwareSystem.list_vm_portgroups()
+    kernel_pgs = map(lambda p: p['Portgroup'],VMwareSystem.list_vmkernel_ports())
+    vm_pg_names = filter(lambda p: not p in kernel_pgs,all_pgs.keys())
+    if len(vm_pg_names) < 2:
         print InstallHelper.boxed_message(['There should be at least two portgroups configured'])
         sys.exit(1)
     print 'Please select your public network:'
-    public_pg = InstallHelper.ask_choice(pg_names)
+    public_pg = InstallHelper.ask_choice(vm_pg_names)
     print 'Please select your storage network:'
-    private_pg = InstallHelper.ask_choice(pg_names)
+    private_pg = InstallHelper.ask_choice(vm_pg_names)
 
     nic_config = VMwareSystem.build_nic_config([private_pg, public_pg])
 
